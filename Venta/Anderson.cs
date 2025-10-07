85cadee3fa48387e5436986ad6f88ebd56e0c195
ANDERSON JAIR DURAND SALAZAR <1619139@senati.pe>
2025-10-07 10:49:07 -0500

    cerrar

---
+++ Venta/Anderson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Venta
+{
+    public partial class Anderson : Form
+    {
+        public Anderson()
+        {
+            InitializeComponent();
+        }
+
+        private void Anderson_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
