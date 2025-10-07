e809d5772e5013cbb9379477df6c93202a4fe1f7
Lab112Pc01 <Lab112Pc01@07CHMK1PA104A05>
2025-10-07 10:43:32 -0500

    hfd

--- Venta/Form1.Designer.cs
+++ Venta/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -38,22 +37,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(319, 295);
+            this.button1.Location = new System.Drawing.Point(445, 236);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(181, 99);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(290, 89);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(227, 133);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // imageList1
             // 
@@ -64,7 +53,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Venta.Properties.Resources.tecnicell_removebg_preview;
-            this.pictureBox1.Location = new System.Drawing.Point(39, 67);
+            this.pictureBox1.Location = new System.Drawing.Point(139, 79);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(199, 245);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -77,7 +66,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -89,7 +77,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
