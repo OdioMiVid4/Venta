353e43344c9faa1b19a864f037189b546cfe918f
Kurxmy <njulcacorrea@gmail.com>
2025-10-07 10:45:01 -0500

    ventas

---
+++ Venta/Formulario ventas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Venta
+{
+    public partial class Formulario_ventas : Form
+    {
+        public Formulario_ventas()
+        {
+            InitializeComponent();
+        }
+    }
+}
